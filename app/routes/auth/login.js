a6c368e3f5ba29efbff9f2e38e0bdbb2992ebf04
Richard Steen <richard.steen@activenetwork.com>
2016-07-12 10:46:21 -0500

    toasty

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,14 +1,33 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  session: Ember.inject.service(),
+const { Route, inject } = Ember;
+
+export default Route.extend({
+
+  session: inject.service(),
+  flashMessages: inject.service(),
+
   actions: {
     doLogin() {
       const user = this.get('currentModel');
       this.get('session')
         .authenticate(
-          'authenticator:peepchat', user.email, user.password
-      );
+          'authenticator:peepchat',
+          user.email,
+          user.password
+        ).then(() => {
+          this.get('flashMessages').success('Logged in!');
+        }).catch((response) => {
+
+          const { errors } = response;
+
+          if (errors.mapBy('code').indexOf(401) >= 0) {
+            this.get('flashMessages')
+              .danger('There was a problem with your username or password, please try again');
+          } else {
+            this.get('flashMessages').danger('ServerError');
+          }
+        });
     }
   },
   model() {
