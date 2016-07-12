c2f3399f30f68c65e29c2e09d9c35a85ed32df50
Richard Steen <richard.steen@activenetwork.com>
2016-07-12 09:50:05 -0500

    so simple auth

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
   actions: {
     doLogin() {
-      alert('login attempted');
+      const user = this.get('currentModel');
+      this.get('session')
+        .authenticate(
+          'authenticator:peepchat', user.email, user.password
+      );
     }
   },
   model() {
