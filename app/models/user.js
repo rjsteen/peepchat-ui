0849956c58dcfabe539059a36f89d750980770f1
Richard Steen <richard.steen@activenetwork.com>
2016-07-15 11:28:00 -0500

    stuff and things

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+// import { belongsTo } from 'ember-data/relationships';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   email: attr('string'),
   password: attr('string'),
-  passwordConfirmation: attr('string')
+  passwordConfirmation: attr('string'),
+  rooms: hasMany('room')
 });
